93720e93aa0fccf144b903e22632170653bd3fa1
kosta2011 <kosta4job@yandex.ru>
2022-02-20 00:46:57 +0500

    Initial commit

---
+++ src/main/java/koschei/models/Deth8.java
@@ -0,0 +1,12 @@
+package koschei.models;
+
+import koschei.KoscheiTheDeathless;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Deth8 {
+
+    @Autowired
+    private KoscheiTheDeathless koscheiTheDeathless;
+}
